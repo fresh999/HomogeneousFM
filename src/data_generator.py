fe9d9f8e93098b17773be4ac34ee9538460348ba
fresh999 <francirusc@gmail.com>
2026-03-06 19:20:10 +0100

    finish implementing invariant model

--- src/data_generator.py
+++ src/data_generator.py
@@ -9,13 +9,11 @@ import matplotlib.ticker as ticker
 
 
 def inf_train_gen(batch_size: int = 200, device: str = 'cpu') -> torch.Tensor:
-    '''
-        Generates checkerboard distribution on the upper half-plane model of hyperbolic space.
-    '''
+    '''Generates checkerboard distribution on the upper half-plane model of hyperbolic space.'''
 
     x = torch.rand(batch_size, device=device) * 6 - 3
     y = torch.rand(batch_size, device=device) - torch.randint(high=3, size=(batch_size,), device=device) * 2
-    y += (torch.floor(x) % 2)
+    y += (torch.floor(x) % 2 + 1)
 
     data = torch.cat([x[:, None], y[:, None]], dim=1) * 0.45
     d = vector_norm(data, dim=1).unsqueeze(-1)
@@ -28,15 +26,28 @@ def inf_train_gen(batch_size: int = 200, device: str = 'cpu') -> torch.Tensor:
 
     return new_data.float()
 
+def section(data: torch.Tensor) -> torch.Tensor:
+    '''Maps points on upper half-plane to symplectic group.'''
+
+    x, y = data[:, 0, None], data[:, 1, None]
+    sqrt_y = torch.sqrt(y)
+    row_1 = torch.cat([sqrt_y, x / sqrt_y], dim=1)
+    row_2 = torch.cat([torch.zeros_like(y), 1 / sqrt_y], dim=1)
+
+    return torch.stack([row_1, row_2], dim=1)
+
+
 
 if __name__ == '__main__':
 
     torch.manual_seed(42)
+    batch_size = 3
 
-    data = inf_train_gen(10000)
+    data = inf_train_gen(batch_size)
+    sec = section(data)
 
-    plt.scatter(data[:, 0].detach(), data[:, 1].detach(), marker='.', alpha=0.5)
-    plt.show()
+    # plt.scatter(data[:, 0].detach(), data[:, 1].detach(), marker='.', alpha=0.5)
+    # plt.savefig('img.pdf')
 
 
 
--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+from math import pi
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -19,9 +21,6 @@ class MLPBlock(nn.Module):
         self.dense_layer = nn.Linear(input_features, output_features)
         self.activation = Activation(activation)
 
-    '''
-        reshape input tensor
-    '''
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         y = self.dense_layer(x)
         return self.activation(y)
@@ -30,14 +29,53 @@ class MLPBlock(nn.Module):
 class MLP(nn.Module):
     def __init__(self, input_features: int, output_features: int, width: int, depth: int, activation: str) -> None:
         super().__init__()
-        self.net = nn.Sequential(
-            MLPBlock(input_features, width, activation),
+        self.input_features = input_features
+        self.model = nn.Sequential(
+            MLPBlock(input_features + 1, width, activation),
             *[MLPBlock(width, width, activation) for _ in range(depth)],
             MLPBlock(width, output_features, None)
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.net(x)
+    def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        sz = x.size()
+        x = x.reshape(-1, self.input_features)
+        t = t.reshape(-1, 1).expand(x.shape[0], 1).float()
+        h = torch.cat([x, t], dim=1)
+        return self.model(h)
+
+
+class InvariantModel(nn.Module):
+    '''
+        Wrapper for a model that makes it invariant under a group action.
+        Only implemented for Sp(2) / U(1).
+    '''
+    def __init__(self, model: nn.Module, n_samples: int = 100) -> None:
+        self.n_samples = n_samples
+        self.model = model
+
+    def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        '''
+            X_inv(p) = \int_{U(1)} Ad_{g^{-1}} (X(g.p)) d mu (g)
+            d mu = Haar measure = d theta / 2 pi
+        '''
+        for _ in range(n_samples):
+            theta = 2 * pi * torch.rand(out.shape[0])
+            sin = torch.sin(theta).unsqueeze(-1)
+            cos = torch.cos(theta).unsqueeze(-1)
+
+
+
+if __name__ == '__main__':
+
+    torch.manual_seed(43)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    model = MLP(2, 2, width=64, depth=2, activation='relu')
+    bs = 10
+
+    x = torch.rand((bs, 2), device=device)
+    t = torch.randint(high=2, size=(bs,), device=device)
+    print(model(x, t))
 
 
 
